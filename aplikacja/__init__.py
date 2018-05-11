fea33b0513a1313d93664480802e6f026c0c0e33
Kotelek <lettynkowa@gmail.com>
2018-05-11 13:07:49 +0200

    work on leszczyna

---
+++ aplikacja/__init__.py
@@ -0,0 +1,11 @@
+#from flask import Flask
+'''
+from config import Config
+'''
+#app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+'''
+app.config.from_object(Config)
+'''
+#from app import views
+
